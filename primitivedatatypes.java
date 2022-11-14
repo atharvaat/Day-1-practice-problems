97480462e46f0ffa851b5ac28a3f1c5b59434e90
atharva <atharvatodankar.at@gmail.com>
2022-11-14 12:45:05 +0530

    TO check primitive data types

---
+++ primitivedatatypes.java
@@ -0,0 +1,25 @@
+package Day1practiceproblem;
+
+public class primitivedatatypes {
+	boolean t;
+	   byte b;
+	   short s;
+	   int i;
+	   long l;
+	   float f;
+	   double d;
+	   void Display() {
+	      System.out.println("boolean (Initial Value) = " + t);
+	      System.out.println("byte (Initial Value) = " + b);
+	      System.out.println("short (Initial Value) = " + s);
+	      System.out.println("int (Initial Value) = " + i);
+	      System.out.println("long (Initial Value) = " + l);
+	      System.out.println("float (Initial Value) = " + f);
+	      System.out.println("double (Initial Value) = " + d);
+	   }
+	   public static void main(String[] args) {
+	      primitivedatatypes d = new primitivedatatypes();
+	      System.out.println("Displaying initial values...");
+	      d.Display();
+	   }
+}
